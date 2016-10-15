e21de9266d392a29c7d7d45eb7c3442402223f43
subdee <info@subdee.org>
2016-10-15 17:51:52 +0200

    Add weather widget

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Lcn\WeatherForecastBundle\Service\Forecast;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +14,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        /** @var Forecast $forecast */
+        $forecast = $this->container->get('lcn.weather_forecast');
+        $forecastForDay = $forecast->getForToday(52.0674480,4.4031030);
+
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'forecast' => $forecastForDay,
         ]);
     }
 }
