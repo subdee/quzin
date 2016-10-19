1cfe58b778e62dd274fdcf34313c73184684af99
Kostas Thermos <konstantinos.thermos@persgroep.nl>
2016-10-19 12:52:19 +0200

    Fix migrations

--- app/DoctrineMigrations/Version20161019070257.php
+++ app/DoctrineMigrations/Version20161019070257.php
@@ -24,7 +24,7 @@ class Version20161019070257 extends AbstractMigration implements ContainerAwareI
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
-        $this->addSql('CREATE TABLE item (id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, image VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE item (id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, image VARCHAR(255) DEFAULT NULL, type INTEGER DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_1F1B251E5E237E06 ON item (name)');
         $this->addSql('CREATE TABLE seasonal (id INTEGER NOT NULL, item_id INTEGER NOT NULL, month INTEGER NOT NULL, PRIMARY KEY(id))');
     }
@@ -79,5 +79,36 @@ class Version20161019070257 extends AbstractMigration implements ContainerAwareI
 
             $entityManager->flush();
         }
+
+        $veggies = [
+            'πορτοκάλια' => Item::TYPE_FRUIT, 'λεμόνια' => Item::TYPE_VEGETABLE, 'μανταρίνια' => Item::TYPE_FRUIT,
+            'γκρέιπ φρουτ' => Item::TYPE_FRUIT, 'μήλα' => Item::TYPE_FRUIT, 'άνηθος' => Item::TYPE_HERB,
+            'μαϊντανός' => Item::TYPE_HERB, 'μπρόκολο' => Item::TYPE_VEGETABLE, 'παντζάρια' => Item::TYPE_VEGETABLE,
+            'πράσα' => Item::TYPE_VEGETABLE, 'ραδίκια' => Item::TYPE_VEGETABLE, 'ράπα' => Item::TYPE_VEGETABLE,
+            'λαχανάκια Βρυξελλών' => Item::TYPE_VEGETABLE, 'λάχανο' => Item::TYPE_VEGETABLE, 'σέλινο' => Item::TYPE_HERB,
+            'σέσκουλο' => Item::TYPE_VEGETABLE, 'σπανάκι' => Item::TYPE_VEGETABLE, 'φινόκιο' => Item::TYPE_VEGETABLE,
+            'αβοκάντο' => Item::TYPE_FRUIT, 'κουνουπίδι' => Item::TYPE_VEGETABLE, 'μαρούλι' => Item::TYPE_VEGETABLE,
+            'καρότα' => Item::TYPE_VEGETABLE, 'σταφίδες' => Item::TYPE_FRUIT, 'καρύδια' => Item::TYPE_NUT, 'αμύγδαλα' => Item::TYPE_NUT,
+            'κάστανα' => Item::TYPE_VEGETABLE, 'αγκινάρες' => Item::TYPE_VEGETABLE, 'αντίδια' => Item::TYPE_VEGETABLE, 'σπαράγγια' => Item::TYPE_VEGETABLE,
+            'αρακάς' => Item::TYPE_VEGETABLE, 'ραπανάκια' => Item::TYPE_VEGETABLE, 'κολοκυθάκια' => Item::TYPE_VEGETABLE, 'κουκιά' => Item::TYPE_VEGETABLE,
+            'κρεμμυδάκι' => Item::TYPE_HERB, 'φράουλες' => Item::TYPE_FRUIT, 'κεράσια' => Item::TYPE_FRUIT, 'βερίκοκα' => Item::TYPE_FRUIT,
+            'μούσμουλα' => Item::TYPE_FRUIT, 'βλίτα' => Item::TYPE_VEGETABLE, 'φασολάκια' => Item::TYPE_VEGETABLE, 'μάραθος' => Item::TYPE_HERB,
+            'πιπεριές' => Item::TYPE_VEGETABLE, 'καρπούζι' => Item::TYPE_FRUIT, 'βύσσινο' => Item::TYPE_FRUIT, 'πεπόνι' => Item::TYPE_FRUIT,
+            'αχλάδια' => Item::TYPE_FRUIT, 'ντομάτες' => Item::TYPE_VEGETABLE, 'αγγούρια' => Item::TYPE_VEGETABLE, 'κρεμμύδια' => Item::TYPE_VEGETABLE,
+            'σκόρδα' => Item::TYPE_VEGETABLE, 'πατάτες' => Item::TYPE_VEGETABLE, 'μελιτζάνες' => Item::TYPE_VEGETABLE, 'σύκα' => Item::TYPE_FRUIT,
+            'ροδάκινα' => Item::TYPE_FRUIT, 'κολοκύθα' => Item::TYPE_VEGETABLE, 'μπάμιες' => Item::TYPE_VEGETABLE, 'δαμάσκηνα' => Item::TYPE_FRUIT,
+            'φυστίκια Αιγίνης' => Item::TYPE_NUT, 'ακτινίδια' => Item::TYPE_FRUIT, 'κυδώνια' => Item::TYPE_FRUIT, 'λωτούς' => Item::TYPE_FRUIT,
+            'ρόδια' => Item::TYPE_FRUIT, 'σταφύλια' => Item::TYPE_FRUIT, 'φουντούκια' => Item::TYPE_NUT, 'μπανάνες' => Item::TYPE_FRUIT
+        ];
+
+        foreach ($veggies as $name => $type) {
+            /** @var Item $item */
+            $item = $entityManager->getRepository('AppBundle:Item')->findOneBy(['name' => $name]);
+            if ($item) {
+                $item->setType($type);
+                $entityManager->persist($item);
+            }
+        }
+        $entityManager->flush();
     }
 }
